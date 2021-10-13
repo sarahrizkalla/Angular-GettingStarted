81e2b2c6f4b1d79d5e9aebbb325a1a0659a03f6a
Sarah Rizkalla <sarah.rizkalla16@gmail.com>
2021-10-13 18:05:29 -0400

    Use data binding and pipes to add interactivity to the ProductList Component

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,6 +6,10 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
     pageTitle: string= 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
     products: any[] = [
         {
             "productId": 2,
@@ -28,4 +32,8 @@ export class ProductListComponent {
             "imageUrl": "assets/images/hammer.png"
         }
     ];
+    // Methods
+    toggleImage(): void {
+       this.showImage = !this.showImage; 
+    }
 }
